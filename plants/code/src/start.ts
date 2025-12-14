59d3a065ebc849c3672b5e84bb2131a857a2981f
Miguel Brugge <brug0024@hz.nl>
2025-12-14 16:54:42 +0100

    Fixed merging issues

--- plants/code/src/start.ts
+++ plants/code/src/start.ts
@@ -1,21 +1,41 @@
-import Express, { Application } from 'express';
+import Express, { Application, Request, Response, NextFunction } from 'express';
 import * as Dotenv from 'dotenv';
 Dotenv.config({ path: '.env' });
-
 import IndexRouter from './routes/index.js';
 import { errorHandler } from './middleware/errors/errorHandler.js';
+import helmet from 'helmet';
+import cors from 'cors';
 
 const app: Application = Express();
-const port: number = process.env.PORT ? parseInt(process.env.PORT) : 3020;
+const port: number = process.env.PORT ? parseInt(process.env.PORT) : 3010;
 
+// security middleware
+app.use(helmet());
+
+// Cors
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
+
+// body parsing
 app.use(Express.json());
 app.use(Express.urlencoded({ extended: true }));
 
-// All routing handled by IndexRouter
 app.use('/', IndexRouter);
 
+// 404
+app.use((req: Request, res: Response, next: NextFunction) => {
+  try {
+    throw new Error('Resource not found', { cause: 404 });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// error handler
 app.use(errorHandler);
 
 app.listen(port, () => {
-  console.log(`🌱 Plants service running → PORT ${port}`);
+  console.log(`🍿 Express running → PORT ${port}`);
 });
