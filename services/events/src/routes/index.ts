e7e6d096c2e89a54dfb5f2901fd007488daed235
Miguel Brugge <brug0024@hz.nl>
2026-01-04 21:35:50 +0100

    Added latest feature

--- services/events/src/routes/index.ts
+++ services/events/src/routes/index.ts
@@ -1,9 +1,10 @@
 import Express, { Router } from 'express';
-import { getEvent, getEvents, createEvent } from '../controllers/eventsController.ts';
+import { getEvent, getEvents, createEvent, deleteEvent } from '../controllers/eventsController.ts';
 const router: Router = Express.Router();
 
 router.get('/', getEvents);
 router.get('/:id', getEvent);
 router.post('/', createEvent);
+router.delete('/:id', deleteEvent);
 
 export default router;
