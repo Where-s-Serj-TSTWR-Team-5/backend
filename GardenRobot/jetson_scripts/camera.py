ab30a04e66793e26a0bb86ec5004b689f177a573
NMarcell0001 <NMarcell0001>
2026-01-12 17:16:50 +0100

    Switched to using YOLO for object detection

--- GardenRobot/jetson_scripts/camera.py
+++ GardenRobot/jetson_scripts/camera.py
@@ -5,10 +5,12 @@ save_dir = "cam_images"
 os.makedirs(save_dir, exist_ok=True)
 
 gst_str = (
-    'nvarguscamerasrc ! '
-    'video/x-raw(memory:NVMM), width=256, height=256, format=NV12, framerate=60/2 ! '
-    'nvvidconv ! videoconvert ! '
-    'video/x-raw, format=BGR ! appsink'
+    "nvarguscamerasrc sensor-id=0 ! "
+    "video/x-raw(memory:NVMM), width=1920, height=1080, format=NV12, framerate=30/1 ! "
+    "nvvidconv ! "
+    "video/x-raw, format=BGRx ! "
+    "videoconvert ! "
+    "video/x-raw, format=BGR ! appsink drop=True"
 )
 
 cap = cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
