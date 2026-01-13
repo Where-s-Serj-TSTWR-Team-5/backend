1c9fa0c76ccb6b291298160514aab7fb365faca1
NMarcell0001 <NMarcell0001>
2026-01-13 19:39:02 +0100

    Stable training pipeline

--- GardenRobot/jetson_scripts/train.py
+++ GardenRobot/jetson_scripts/train.py
@@ -3,10 +3,11 @@ from ultralytics import YOLO
 model = YOLO('yolov8n.pt')
 
 model.train(data='/home/hzjarvis/pycharmgithub/GardenRobot/jetson_scripts/dataset/data.yaml',
-            epochs = 50,
-            imgsz =640,
+            epochs = 1,
+            imgsz =320,
             device=0,
-            batch=4,
+            batch=1,
             plots=True,
             cache=False,
-            amp=False)
+            amp=False,
+            workers=0)
