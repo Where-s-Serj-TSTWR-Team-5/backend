783e282c4210e6484f79f1930c4e96f835f2699d
Miguel Brugge <brug0024@hz.nl>
2026-01-15 17:55:46 +0100

    Added label and enums for event categories to database

--- database/src/index.ts
+++ database/src/index.ts
@@ -5,5 +5,12 @@ export const ROLES = {
   USER: "USER",
   GREEN_OFFICE_MEMBER: "GREEN_OFFICE_MEMBER",
 } as const;
-
 export type Role = (typeof ROLES)[keyof typeof ROLES];
+
+export const EVENT_CATEGORIES = {
+  SUSTAINABILITY: 'SUSTAINABILITY',
+  GARDENING: 'GARDENING',
+  WORKSHOP: 'WORKSHOP',
+  CLEAN_UP: 'CLEAN_UP',
+} as const;
+export type EventCategory = (typeof EVENT_CATEGORIES)[keyof typeof EVENT_CATEGORIES];
